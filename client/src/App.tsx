bf279295c4d162267ac777383777c6f8cff2212d
Volodymyr Reient <volodymyr.reient@softblues.io>
2023-09-14 10:50:50 +0300

    change default antd font to Lato

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConfigProvider } from "antd";
 import Home from "./pages/Home";
 import "antd/es/style/reset.css";
 
@@ -9,9 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "Lato",
+            },
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </ConfigProvider>
       </QueryClientProvider>
     </BrowserRouter>
   );
